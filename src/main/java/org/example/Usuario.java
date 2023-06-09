a73e27ec41c0a86bf2661af122cb38456853dc2f
angeljpr6 <angelp.r806@gmail.com>
2023-06-09 13:28:14 +0100

    Cambio de double a float en la variable precioTotal de Cesta ComprarCesta casi creado del todo VerCompra terminado (sin comprobar)

--- src/main/java/org/example/Usuario.java
+++ src/main/java/org/example/Usuario.java
@@ -1,8 +1,8 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class Usuario {
@@ -43,5 +43,89 @@ public class Usuario {
             System.out.println("Ups... algo ha fallado");
         }
     }
+    /**
+     * Te enseña la Compra de ticket en especifico
+     *
+     * @param ticket
+     */
+    public static void verCompra(int ticket){
+        try {
+            PreparedStatement stm = c.prepareStatement("select * from Compra where ticket=?;");
+            stm.setInt(1,ticket);
+            ResultSet result = stm.executeQuery();
+            System.out.println("Ticket: "+result.getInt("ticket"));
+            System.out.println("Fecha y Hora de compra: "+result.getDate("fecha")+" "+result.getTime("hora"));
+            System.out.println("Precio Total: "+result.getFloat("precio"));
+            System.out.println("Estado: "+result.getString("estado"));
+        } catch (SQLException e) {
+            System.out.println("Ups... algo ha fallado");
+        }
+    }
 
+    /**
+     * Hace la compra de una cesta a nombre del usuario
+     * @param cesta
+     */
+    public void comprarCesta(Cesta cesta){
+        try {
+            PreparedStatement stm = c.prepareStatement("insert into Compras values(default,?,?,?,?,?,?);");
+            stm.setDate(1, Date.valueOf(LocalDate.now()));
+            stm.setTime(2,Time.valueOf(LocalTime.now()));
+            stm.setFloat(3,cesta.getPrecioTotal());
+            stm.setInt(4,cesta.getId());
+            stm.setInt(5,this.id);
+            stm.setString(6,"");
+        } catch (SQLException e) {
+            System.out.println("Algo ha ido mal");
+        }
+    }
+
+    // GETTER & SETTER
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Cesta getCesta() {
+        return cesta;
+    }
+
+    public void setCesta(Cesta cesta) {
+        this.cesta = cesta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
