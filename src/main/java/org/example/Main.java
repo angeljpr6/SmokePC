c21e0f0af349d9e4e26558450851e931c16f5062
Raúl <raromar2014@gmail.com>
2023-06-09 11:21:19 +0100

    Productos hecho +o-

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,7 @@ public class Main {
         System.out.println("He!");
 
         Principal.initConection();
-        TarjetaCredito tarj =new TarjetaCredito(484373795,"Angel Perez",222,1);
-        Usuario.anadirMetodoPago(tarj);
+
 
     }
 }
