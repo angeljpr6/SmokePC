d3ca6bd695233887c569c606fc92900fd7260b7d
Raúl <raromar2014@gmail.com>
2023-06-09 13:22:46 +0100

    CompraPepo  commit

--- src/main/java/org/example/Compra.java
+++ src/main/java/org/example/Compra.java
@@ -1,9 +1,33 @@
 package org.example;
-
+import java.sql.*;
 public class Compra {
     private String fecha;
     private String hora;
-    private  int tickect;
+    private int tickect;
     private double precio;
+    private static String[] estado = {"tramitacion", "enviado", "rechazado", "llegando"};
 
+    public static  void cambiarEstado(String estadoActual, boolean cestaConfirmada) {
+        if (cestaConfirmada) {
+            if (estado[0].equals(estadoActual)) { // "tramitacion"
+                estadoActual = estado[3]; // "llegando"
+                System.out.println("Estado de la compra actualizado a 'llegando'");
+            } else {
+                System.out.println("La compra ya ha sido confirmada o tiene un estado inválido");
+            }
+        } else {
+            if (estado[0].equals(estadoActual)) { // "tramitacion"
+                estadoActual = estado[2]; // "rechazado"
+                System.out.println("Estado de la compra actualizado a 'rechazado'");
+            } else {
+                System.out.println("La compra ya ha sido confirmada o tiene un estado inválido");
+            }
+        }
+
+        if (estado[3].equals(estadoActual)) { // "llegando"
+            estadoActual = estado[1]; // "enviado"
+            System.out.println("Estado de la compra actualizado a 'enviado'");
+            System.out.println("El envío se ha efectuado correctamente");
+        }
+    }
 }
---
+++ src/main/java/org/example/EstadoCompra.java
@@ -0,0 +1,13 @@
+package org.example;
+/*
+Esto es tan inutil como joni viniendo a clase
+ */
+public enum EstadoCompra {
+    tramitacion(),
+    enivado,
+    rechazado,
+    llegando;
+
+
+
+}
