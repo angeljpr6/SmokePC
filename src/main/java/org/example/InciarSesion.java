eee67ee79c15455f1e315863db40fa75a7d183b9
angeljpr6 <angelpr.dam@gmail.com>
2023-06-12 19:58:41 +0100

    Interfaz Iniciar Sesion Diseñado

--- src/main/java/org/example/InciarSesion.java
+++ src/main/java/org/example/InciarSesion.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import javax.swing.*;
+
 public class InciarSesion {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton iniciarButton;
+    private JButton crearCuentaButton;
+    private JButton pulsaAquiButton;
 }
