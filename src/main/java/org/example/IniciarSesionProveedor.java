66801b79b22f0ba0339f73bb09571f1c66621ad5
danielperezalejandro <danieloctaviosegundo@gmail.com>
2023-06-13 23:16:13 +0100

    interfaz inicio proveedor

---
+++ src/main/java/org/example/IniciarSesionProveedor.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import javax.swing.*;
+
+public class IniciarSesionProveedor {
+    private JTextField correo;
+    private JPasswordField passwordField1;
+    private JButton volverAtrasButton;
+    private JButton iniciarButton;
+}
