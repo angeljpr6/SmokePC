5fb5ebd2d3fcd5d1d7e663a500c5178c2b07d50c
angeljpr6 <angelp.r806@gmail.com>
2023-06-13 12:51:33 +0100

    En proceso PantallaPrincipalUsuario

--- src/main/java/org/example/InciarSesion.java
+++ src/main/java/org/example/InciarSesion.java
@@ -1,14 +1,33 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class InciarSesion {
-    private JPanel panel1;
+public class InciarSesion extends JFrame {
+
     private JTextField correoElectronico;
     private JPasswordField contraseña;
     private JButton iniciarButton;
     private JButton crearCuentaButton;
     private JButton esProveedorButton;
+    private JPanel panelPrincipal;
+    public InciarSesion(){
+        this.setContentPane(panelPrincipal);
+        this.pack();
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new InciarSesion().setVisible(true);
+
+            }
+        });
+    }
+
+
 
 
 
---
+++ src/main/java/org/example/PantallaPrincipalUsuario.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import javax.swing.*;
+
+public class PantallaPrincipalUsuario {
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JPanel producto;
+    private JScrollBar scrollBar1;
+}
